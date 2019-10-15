61cdd75d821edd6d0c851dc310359784ccff985c
song3986 <56580319+song3986@users.noreply.github.com>
2019-10-15 19:38:42 +0900

    Update 01.Analog_LED.ino
    More blink

--- Advanced/01.Analog_LED/01.Analog_LED.ino
+++ Advanced/01.Analog_LED/01.Analog_LED.ino
@@ -5,5 +5,10 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  analogWrite(3,10);
+  analogWrite(3,85);
+  delay(200);
+  analogWrite(3,170);
+  delay(200);
+  analogWrite(3,255);
+  delay(200);
 }
